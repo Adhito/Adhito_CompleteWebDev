a20c681517d2a33e433c04a496d7baea8f83178b
Adhito <45397405+Adhito@users.noreply.github.com>
2019-02-20 14:22:12 +0700

    Add files via upload

---
+++ 11_JavaScript/115_JavaScriptTerminology/notes-0001.js
@@ -0,0 +1,45 @@
+
+
+// Function Declaration
+function newFunction(){
+
+}
+
+// Function Expression
+var newFunction = function nameFunction(){
+
+};
+
+
+// Expression
+
+	1+3;
+	var a = 1;
+	return true;
+
+
+// Calling or invoking a function
+	alert();
+	newFunction(PARAMETER1, PARAMETER2);
+
+
+// Assigning a variable
+	var a = 2;
+
+
+// This is a function 
+function thisIsAFunction(){
+
+}
+	// How to access/call it
+	thisIsAFunction();
+
+
+// This is a method
+var obj = {
+	thisIsAMethod: function(){
+
+	}
+}
+	// How to access/call it
+	obj.thisIsAMethod();
