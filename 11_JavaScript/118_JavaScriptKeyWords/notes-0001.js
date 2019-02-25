df5bc2de646b3ea5f9ccc274f8405727863e8b34
Adhito <45397405+Adhito@users.noreply.github.com>
2019-02-25 12:24:18 +0700

    Add files via upload

---
+++ 11_JavaScript/118_JavaScriptKeyWords/notes-0001.js
@@ -0,0 +1,9 @@
+
+JAVASCRIPT KEYWORDS
+-------------------
+
+/*
+	NOTES:
+	Javascript has also keywords like those in C or C++, In C we ussualy know
+	them as reserved keywords and these are the words that we cannot use 
+*/
