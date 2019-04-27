c44c4c802c25e8065890db2c42e134b52467690a
Adhito <45397405+Adhito@users.noreply.github.com>
2019-04-27 20:30:18 +0700

    Upload Files Section 143

---
+++ 13_JavaScript_Advanced/143_ES7/SectionOutline.js
@@ -0,0 +1,12 @@
+
+
+/* .includes	-- Check if a string/array include a certain
+					parameter and it return true or false*/
+
+'Hellooo'.includes('0');
+// Should return true;
+
+
+/*  Exponential operator	-- Explained below*/
+const square = (x) => x**2;
+const cube = (y) => y**3; 
